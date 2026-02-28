081f8d5384ddb8b653cd95e34eb64908add8517a
Elizabeth Henry <elirhenry@Elizabeths-iMac.local>
2026-02-28 12:38:44 -0700

    Added 2nd project video. Add 3rd project video.

--- frontend/src/Media/projectsData.js
+++ frontend/src/Media/projectsData.js
@@ -1,16 +1,16 @@
 export const projects = [
   {
-    id: "lightsaber",
+    id: "fargate",
     name: "Fargate",
     description: "A fictional space travel web application with user friendly navigation features and a secure database to store user information.",
     videoUrl: "/videos/Fargate-Demo.mp4",
   },
-  // {
-  //   id: "lightsaber",
-  //   name: "Saber-tory",
-  //   description: "A lightsaber inventory tool for a retail database. Displays a general inventory list for quick access and provides editing features for authenticated users.",
-  //   videoUrl: "/videos/Test-Recording.mp4",
-  // },
+  {
+    id: "sabertory",
+    name: "Saber-tory",
+    description: "A Star Wars themed lightsaber inventory tool for a retail database. Displays a read-only view for quick access and provides editing features for authenticated users.",
+    videoUrl: "/videos/Sabertory-Demo.mp4",
+  },
   // {
   //   id: "lightsaber",
   //   name: "Dish Dash",
